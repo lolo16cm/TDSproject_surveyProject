b5b624a818fbbdcfc775e5ac1365f811dc481a7f
CaroluvCats <86131525+lolo16cm@users.noreply.github.com>
2024-12-02 21:54:08 -0500

    Delete create_form directory

--- create_form/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Choices, Questions, Form
-
-admin.site.register(Choices)
-admin.site.register(Questions)
-admin.site.register(Form)
